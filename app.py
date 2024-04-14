a68ac99ead4cc77b6ff2a2cb6dac3ee0269cce6c
jeff <jeffshen.37@gmail.com>
2024-04-14 13:51:15 -0400

    pages setup pre-predictions

--- app.py
+++ app.py
@@ -4,9 +4,17 @@ import numpy as np
 
 app = Flask(__name__)
 
+df = pd.read_csv('searches.csv')
+
+print(df.head())
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/team')
+def team():
+    return render_template('team.html')
 
 @app.route('/data')
 def data():
@@ -15,3 +23,6 @@ def data():
 @app.route('/predictions')
 def predictions():
     return render_template('predictions.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
