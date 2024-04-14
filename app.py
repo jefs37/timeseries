cd6916000b5867fef4850247877a832b3e920dbb
jeff <jeffshen.37@gmail.com>
2024-04-14 16:01:32 -0400

    first draft with predictions

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pandas as pd
 import numpy as np
 
 app = Flask(__name__)
 
 df = pd.read_csv('searches.csv')
-
-print(df.head())
 
 @app.route('/')
 def index():
@@ -20,8 +18,14 @@ def team():
 def data():
     return render_template('data.html')
 
-@app.route('/predictions')
+@app.route('/timeseries')
+def timeseries():
+    return render_template('timeseries.html')
+
+@app.route('/predictions', methods = ['POST', 'GET'])
 def predictions():
+    if request.method == 'POST':
+        return render_template('predictions.html')
     return render_template('predictions.html')
 
 if __name__ == "__main__":
